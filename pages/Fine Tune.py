27425d2cebed13f6d32868a6295ee3728db75609
Quera <70485959+Quera-fr@users.noreply.github.com>
2025-02-13 15:26:44 +0100

    Update Fine Tune.py

--- pages/Fine Tune.py
+++ pages/Fine Tune.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.title("Fine Tune OpenAI")
+
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    st.write(uploaded_file)
+    bytes_data = uploaded_file.getvalue()
