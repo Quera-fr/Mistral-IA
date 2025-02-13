385ee73738cafc755f950e8334f9ac3a4a6f05f9
Quera <70485959+Quera-fr@users.noreply.github.com>
2025-02-13 15:30:24 +0100

    Update Fine Tune.py

--- pages/Fine Tune.py
+++ pages/Fine Tune.py
@@ -1,10 +1,20 @@
 import streamlit as st
+from openai import OpenAI
 
-st.title("Fine Tune OpenAI")
+st.title("Fine Tuning with OpenAI")
 
+api_key = st.input_text("Your API_KEY")
 
+client = OpenAI(api_key=api_key)
+
+# Préparation des données pour l'entrainement d'un modèle ChatCompletion
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     # To read file as bytes:
     st.write(uploaded_file)
     bytes_data = uploaded_file.getvalue()
+
+    upload_file = client.files.create(
+          file=bytes_data,
+          purpose="fine-tune"
+        )
